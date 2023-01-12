0da62fabec93eca27546f1643a6beb07bfe46a97
Salapuddin <vsalapuddin@ufl.edu>
2023-01-11 22:28:59 -0500

    centering the contents

--- src/Home.js
+++ src/Home.js
@@ -9,13 +9,37 @@ import Projects from "./Pages/Projects";
 function Home() {
   return (
     <Parallax pages={3} style={{ top: "0", left: "0" }}>
-      <ParallaxLayer offset={0} speed={2.5}>
+      <ParallaxLayer
+        offset={0}
+        speed={2.5}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Intro />
       </ParallaxLayer>
-      <ParallaxLayer offset={1} speed={2.5}>
+      <ParallaxLayer
+        offset={1}
+        speed={2.5}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Experience />
       </ParallaxLayer>
-      <ParallaxLayer offset={2} speed={2.5}>
+      <ParallaxLayer
+        offset={2}
+        speed={2.5}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Projects />
       </ParallaxLayer>
     </Parallax>
